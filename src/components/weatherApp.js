69bbf6f2cc664146554940f86b7c829735700555
JAL <jalejandromogro1@gmail.com>
2022-07-30 18:59:38 -0400

    config api

--- src/components/weatherApp.js
+++ src/components/weatherApp.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import WeatherForm from "./weatherForm";
+
 function WeatherApp() {
+  const [weather,setWeather]= useState(null);
+  
+  async function loadInfo(city='london') {
+    try {
+      const request = await fetch(
+        `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`
+      );
+
+      const json= await request.json();
+
+      console.log(json); 
+    
+    } catch (error) {
+      
+    }
+  }
+
+  function handleChangeCity(city) {
+    setWeather(city);
+    loadInfo(city);
+  }
   return (
     <div>
-      
+      <WeatherForm onChangeCity={handleChangeCity}/>
     </div>
   );
 }
